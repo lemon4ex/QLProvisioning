804a8947f3711468b76467883682d6364061eca2
lemon4ex <netchen@vip.qq.com>
2017-07-07 13:57:47 +0800

    注释头文件

--- Provisioning/OCSP/SecOCSPResponse.c
+++ Provisioning/OCSP/SecOCSPResponse.c
@@ -1,10 +1,29 @@
-//
-//  SecOCSPSingleResponse.c
-//  OCSPDemo
-//
-//  Created by zeejun on 14-8-12.
-//  Copyright (c) 2014年 zeejun. All rights reserved.
-//
+/*
+ * Copyright (c) 2008-2009 Apple Inc. All Rights Reserved.
+ *
+ * @APPLE_LICENSE_HEADER_START@
+ *
+ * This file contains Original Code and/or Modifications of Original Code
+ * as defined in and that are subject to the Apple Public Source License
+ * Version 2.0 (the 'License'). You may not use this file except in
+ * compliance with the License. Please obtain a copy of the License at
+ * http://www.opensource.apple.com/apsl/ and read it before using this
+ * file.
+ *
+ * The Original Code and all software distributed under the License are
+ * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
+ * Please see the License for the specific language governing rights and
+ * limitations under the License.
+ *
+ * @APPLE_LICENSE_HEADER_END@
+ */
+
+/*
+ * SecOCSPResponse.c - Wrapper to decode ocsp responses.
+ */
 
 #include <stdio.h>
 #include "SecOCSPResponse.h"
